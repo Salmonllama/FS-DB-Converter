e1fcab7fc2c1cade5bf59c4fbb8a92e80035bb23
Salmonllama <a.gryczewski999@gmail.com>
2020-07-27 02:55:40 -0400

    code (WIP)

---
+++ lib/outfit.go
@@ -0,0 +1,43 @@
+package lib
+
+import "fmt"
+
+type Outfit struct {
+	Id string `gorethink:"id"`
+	Link string `gorethink:"link"`
+	Submitter string `gorethink:"submitter"`
+	Tag string `gorethink:"tag"`
+	Meta string
+	Created int64
+	Updated int64
+	Deleted bool
+	Featured bool
+	DisplayCount int
+	DeleteHash string
+}
+
+func (outfit *Outfit) String() string {
+	return fmt.Sprintf("Outfit: [" +
+		"id: %v, " +
+		"link %v, " +
+		"submitter: %v, " +
+		"tag: %v, " +
+		"meta: %v, " +
+		"created: %v" +
+		"updated: %v, " +
+		"deleted: %v, " +
+		"featured: %v, " +
+		"display count: %v, " +
+		"delete hash: %v]",
+		outfit.Id,
+		outfit.Link,
+		outfit.Submitter,
+		outfit.Tag,
+		outfit.Meta,
+		outfit.Created,
+		outfit.Updated,
+		outfit.Deleted,
+		outfit.Featured,
+		outfit.DisplayCount,
+		outfit.DeleteHash)
+}
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/salmonllama/fs-db-converter/lib"
+	"strings"
+
+	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	sqlDb, err := sql.Open("sqlite3", "./fsbot.db")
+	if err != nil {
+		fmt.Print(err)
+	}
+	createTable(sqlDb)
+
+	rethinkDb, err := r.Connect(r.ConnectOpts{
+		Address: "localhost:28015",
+		Database: "fsbot",
+	})
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	rows, err := r.Table("outfits").Run(rethinkDb)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var outfits []lib.Outfit
+	rows.All(&outfits)
+	fmt.Println(getIdFromLink(outfits[0].Link))
+	//for _, outfit := range outfits {
+	//
+	//}
+}
+
+func createTable(db *sql.DB) {
+	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS outfits (id TEXT, link TEXT, submitter TEXT, tag TEXT, meta TEXT, created TEXT, updated TEXT, deleted TEXT, featured TEXT, display_count INT, delete_hash TEXT)")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	_, err = statement.Exec()
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+func getIdFromLink(link string) string {
+	s := strings.Split(link, "/") // https:  i.imgur.com <id>.png
+	id := strings.Split(s[3], ".")[0]
+	return id
+}
